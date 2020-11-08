213e0d5da57e35665f309bf57ea6df04fa161d28
Alexander Leffringhausen <alexander.leffringhausen@web.de>
2020-11-08 19:17:14 +0100

    - Renamed some variables 	- SEGMENTLEDS -> LEDSPERSEGMENT 	- bool digits [][] -> bool symbols[][] 	- digit_show [] -> symbol_show[] + ADD new symbols: C, ° + ADD lots of comments for nearly every line of code + ADD if-condition to easy activate debug text output + ADD temperatur read

--- ard_Digital_Clock_main/ard_Digital_Clock_main.ino
+++ ard_Digital_Clock_main/ard_Digital_Clock_main.ino
@@ -1,8 +1,8 @@
-#define DIGITS 4 //Count available digits
+#define DIGITS 4 //Count available digits on clock
 #define SEGMENTS 7 //Count segments for ONE digit
-#define SEGMENTLEDS 6 //Count LEDs building onto ONE segment
+#define LEDSPERSEGMENT 6 //Count LEDs building onto ONE segment
 #define NUMBERS 10 //Count available digits 0-9
-#define DIGITLEDS SEGMENTS*SEGMENTLEDS //Count LED's per Digit
+#define DIGITLEDS SEGMENTS*LEDSPERSEGMENT //Count LED's per Digit
 
 #include <Adafruit_NeoPixel.h>
 #include <RTClib.h>
@@ -21,25 +21,27 @@
 
 
 //Constants
-//Contains state for segments to display digits [digits][segments]
-                     //SegmentNum: 0 1 2 3 4 5 6 
-bool digits [NUMBERS][SEGMENTS] = {{1,1,1,0,1,1,1}, //0
-                                  {1,0,0,0,1,0,0}, //1
-                                  {1,1,0,1,0,1,1}, //2
-                                  {1,1,0,1,1,1,0}, //3
-                                  {1,0,1,1,1,0,0}, //4
-                                  {0,1,1,1,1,1,0}, //5
-                                  {0,1,1,1,1,1,1}, //6
-                                  {1,1,0,0,1,0,0}, //7
-                                  {1,1,1,1,1,1,1}, //8
-                                  {1,1,1,1,1,1,0}};//9
-
+//Contains state for segments to display digits and symbols [][segments]
+                      //SegmentNum: 0 1 2 3 4 5 6   Number
+bool symbols [][SEGMENTS] = {{1,1,1,0,1,1,1}, //0
+                             {1,0,0,0,1,0,0}, //1
+                             {1,1,0,1,0,1,1}, //2
+                             {1,1,0,1,1,1,0}, //3
+                             {1,0,1,1,1,0,0}, //4
+                             {0,1,1,1,1,1,0}, //5
+                             {0,1,1,1,1,1,1}, //6
+                             {1,1,0,0,1,0,0}, //7
+                             {1,1,1,1,1,1,1}, //8
+                             {1,1,1,1,1,1,0}, //9
+                             {0,1,1,0,0,1,1}, // C
+                             {1,1,1,1,0,0,0}};// °
 
 //Variables
-//Contains variables
-byte digit_show [DIGITS];
-bool pix_show[NUMPIXELS];
-unsigned long refreshtime;
+//Contains variables 
+byte symbol_show [DIGITS];   //Contains numbers to show on clock
+bool pix_show[NUMPIXELS];   //Contains 0/1 (OFF/ON) for each LED/pixel on LED strip
+unsigned long refreshtime;  //Variable to track passed time, to refresh display
+
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
 // strips you might need to change the third parameter -- see the
@@ -52,13 +54,13 @@ void setup() {
   Serial.begin(4800); //Initialize Serial Monitor
   pixels.begin(); // Initialize NeoPixel strip object (REQUIRED)
 
-  if (! rtc.begin()) {
+  if (! rtc.begin()) { //Tests if RTC is connected
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
   }
 
-  if (rtc.lostPower()) {
+  if (rtc.lostPower()) { //Tests if RTC lost power since last update. If lost new time is set to compile time.
     Serial.println("RTC lost power, let's set the time!");
     // When time needs to be set on a new device, or after a power loss, the
     // following line sets the RTC to the date & time this sketch was compiled
@@ -67,49 +69,69 @@ void setup() {
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
-  
+
+  //Dev-place start
   delay(100);
   
-  digit_show[3] = 0;
-  digit_show[2] = 0;
-  digit_show[1] = 0;
-  digit_show[0] = 0;
+  symbol_show[3] = 2;
+  symbol_show[2] = 6;
+  symbol_show[1] = 10;
+  symbol_show[0] = 11;
+
+  //Def-place end
+
+  //Add function for startup Visualisation
   
-  refreshtime = millis();
+  refreshtime = millis(); //Sets actual time to track passed time for refresh display
 
 }
 
 void loop() {
-  if(millis() - refreshtime > 1000){
-    DateTime  now = rtc.now();
 
+  //Add change between time and temperature
+  
+  if(millis() - refreshtime > 1000){    //Check if refreshtime is over
+    DateTime  now = rtc.now();     //Read actual Real Time
+
+  //Set "symbol_show" to show actual Real Time
     byte h = now.hour();
     byte m = now.minute();
-    digit_show[3] = h / 10;
-    digit_show[2] = h - digit_show[3] * 10;
-    digit_show[1] = m / 10;
-    digit_show[0] = m - digit_show[1] * 10;
+    symbol_show[3] = h / 10;
+    symbol_show[2] = h - symbol_show[3] * 10;
+    symbol_show[1] = m / 10;
+    symbol_show[0] = m - symbol_show[1] * 10;
 
-    /*
-    // DEBUG
-    Serial.print(digit_show[3]);
-    Serial.print(digit_show[2]);
-    Serial.print(":");
-    Serial.print(digit_show[1]);
-    Serial.println(digit_show[0]);
-    */
-
-    calc_pixels(digits, digit_show, pix_show);
-
-    pixels.clear();
+    if(false){
+      // DEBUG
+      Serial.print(symbol_show[3]);
+      Serial.print(symbol_show[2]);
+      Serial.print(":");
+      Serial.print(symbol_show[1]);
+      Serial.println(symbol_show[0]);
+    }
     
+
+
+    calc_pixels(symbols, symbol_show, pix_show);  //Calculates every pixel based on "pixel_show"
+
+    pixels.clear(); //Clears all pixels (not sure if needed, if I overwrite every pixel with new informations)
+
+    //Set pixel OFF/ON based on "pix_show" and their color atm fixed to one color. Could implement array to set different color for each pixel.
     for(int i = 0; i < sizeof(pix_show); i++){
       pixels.setPixelColor(i, pixels.Color(0, 150, 0));
     }
 
-    pixels.show();
+    pixels.show();    //Show the pixel brightness and color set
     
-    refreshtime = millis();
+    refreshtime = millis(); //reset refreshtime to wait again
     //Serial.println("Done");
+
+    if(false){
+      //Testplace start
+      Serial.print("Temperature: ");
+      Serial.print(rtc.getTemperature());
+      Serial.println(" C°");
+      //Testplace end
+    }
   }
 }
--- ard_Digital_Clock_main/functions_brightness.h
+++ ard_Digital_Clock_main/functions_brightness.h
@@ -1,17 +1,20 @@
+//Function overview
+void calc_pixels(bool symbols [][SEGMENTS], byte symbol_show[], bool *pix_show);
+//Add multiple functions to change color for each pixel
 
-void calc_pixels(bool digits [][SEGMENTS], byte digit_show[], bool *pix_show);
 
-void calc_pixels(bool digits [][SEGMENTS], byte digit_show[], bool *pix_show){
-  Serial.begin(4800);
+//Functions
+void calc_pixels(bool symbols [][SEGMENTS], byte symbol_show[], bool *pix_show){
   for(int d = 0; d < DIGITS; d++){
     for(int seg = 0; seg < SEGMENTS; seg++){
-      for(int p = 0; p < SEGMENTLEDS; p++){
-        pix_show[SEGMENTLEDS*(d*SEGMENTS+seg)+p] = digits[digit_show[d]][seg];
+      for(int p = 0; p < LEDSPERSEGMENT; p++){
+        pix_show[LEDSPERSEGMENT*(d*SEGMENTS+seg)+p] = symbols[symbol_show[d]][seg];
         
         
         
         /* **DEBUG**
-        //Printout entire pix_show Array with Comments
+        //Printout entire pix_show Array with Comments (digit|segment|pixel|val 1/0)
+        Serial.begin(4800);
         Serial.print("Digit: "); 
         Serial.print(d); 
         Serial.print(" | Seg: "); 
@@ -19,7 +22,7 @@ void calc_pixels(bool digits [][SEGMENTS], byte digit_show[], bool *pix_show){
         Serial.print(" | Pixel: "); 
         Serial.print(p);
         Serial.print(" | Val: ");
-        Serial.println(digits[digit_show[d]][seg]);
+        Serial.println(symbols[symbol_show[d]][seg]);
         */
       }
     }
